69ea7813fcb92854c6ce37e4e904d6669f24762e
BoukayouaLoubna <loubnaboukayoua@gmail.com>
2024-09-16 12:18:49 +0100

    app file modified

--- app.py
+++ app.py
@@ -124,6 +124,10 @@ def predict():
            logger.error(f"Error Occurred During Prediction Process: {e}")
            return render_template('index.html', error_message=f"An error occurred: {str(e)}",cat_cols=cat_cols, num_cols=form_data)
 
+#if __name__ == '__main__':
+ #   logger.debug("Flask app Started")
+  #  app.run(debug=True)
+
 if __name__ == '__main__':
-    logger.debug("Flask app Started")
-    app.run(host='0.0.0.0', debug=True)
+    logger.debug("Starting the Flask app.")
+    app.run(host='0.0.0.0', port=5000, debug=True)
